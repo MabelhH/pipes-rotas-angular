70a05e34146ba7b02ab3fd325aa41a7b03ad975f
clara23 <clarahuanaco20@gmail.com>
2023-01-13 09:37:56 -0500

    segunda actulisacion

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent  implements OnInit{
+input!:string[];
 
+constructor(private datos:DatosService){}
+
+ngOnInit(): void {
+  //this.datos.getAll().subscribe(datos =>console.log(datos));
+  this.datos.getAll().subscribe(datos =>this.input=datos);
+}
 }
--- src/app/servicios/datos.service.ts
+++ src/app/servicios/datos.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatosService {
 
-  constructor() { }
+  private urlApi= 'https://jsonplaceholder.typicode.com/todos';
+
+  constructor(private https:HttpClient) {}
+
+  getAll():Observable<string[]>{
+    return this.https.get<string[]>(this.urlApi)
+  }
 }
